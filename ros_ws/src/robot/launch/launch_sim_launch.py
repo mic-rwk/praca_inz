ae3ec60c7928c025bb4cf33be588405f9d5c7471
mic-rwk <michalrywak@gmail.com>
2025-07-22 18:50:32 +0000

    Add nodes for controllers

--- ros_ws/src/robot/launch/launch_sim_launch.py
+++ ros_ws/src/robot/launch/launch_sim_launch.py
@@ -31,11 +31,22 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_controller"],
+    )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
