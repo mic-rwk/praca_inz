cff02832f3706efcce7a16d1a1ca5ed22e83e771
mic-rwk <michalrywak@gmail.com>
2025-07-30 18:39:22 +0000

    Create publisher for /scan_data, for publishing only ranges

--- ros_ws/src/robot/nodes/ReadingLaser.cpp
+++ ros_ws/src/robot/nodes/ReadingLaser.cpp
@@ -6,6 +6,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
 
 using std::placeholders::_1;
 
@@ -16,16 +17,24 @@ public:
 
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
       "scan", 10, std::bind(&ReadingLaser::topic_callback, this, _1));
+
+      publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("scan_data", 10);
   }
 
 private:
   void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr _msg) const {
 
+    std_msgs::msg::Float64MultiArray laser_scan_output;
+
     for(auto & msg : _msg->ranges){
       RCLCPP_INFO(this->get_logger(), "Range : %f", msg);
+      laser_scan_output.data.push_back(static_cast<double>(msg));
     }
+    publisher_->publish(laser_scan_output);
   }
+  
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher_;
 };
 
 int main(int argc, char *argv[]) {
