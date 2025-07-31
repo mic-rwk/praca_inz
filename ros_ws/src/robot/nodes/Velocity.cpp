b125ef450d0235f74d51ba1203431499e4485ecd
mic-rwk <michalrywak@gmail.com>
2025-07-31 14:24:49 +0000

    Add publisher for linear and angular velocity of robot

--- ros_ws/src/robot/nodes/Velocity.cpp
+++ ros_ws/src/robot/nodes/Velocity.cpp
@@ -6,6 +6,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
 
 using std::placeholders::_1;
 
@@ -16,6 +17,8 @@ public:
 
     subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "odom", 10, std::bind(&Velocity::topic_callback, this, _1));
+
+      publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("robot_velocity_data", 10);
   }
 
 private:
@@ -25,8 +28,15 @@ private:
     double angular_vel = _msg->twist.twist.angular.z;
 
     RCLCPP_INFO(this->get_logger(), "Linear vel: %f\tAngular vel: %f", linear_vel, angular_vel);
+
+    std_msgs::msg::Float64MultiArray robot_velocity_output;
+    robot_velocity_output.data.push_back(linear_vel);
+    robot_velocity_output.data.push_back(angular_vel);
+
+    publisher_->publish(robot_velocity_output);
   }
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher_;
 };
 
 int main(int argc, char *argv[]) {
