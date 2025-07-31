b920ed3eea6611fe0fd24ebaced2167a052c0447
mic-rwk <michalrywak@gmail.com>
2025-07-31 14:26:33 +0000

    Listen proper topics in ros bag

--- ros_ws/src/robot/launch/launch_sim_launch.py
+++ ros_ws/src/robot/launch/launch_sim_launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
     )
 
     rosbag_record = ExecuteProcess(
-    cmd=['ros2', 'bag', 'record', '-o', './src/robot/bag_files/data1', '/scan', '/odom', '/joint_states'],
+    cmd=['ros2', 'bag', 'record', '-o', './src/robot/bag_files/data1', '/scan_data', '/robot_velocity_data', '/encoder_data'],
     output='screen'
 )
 
